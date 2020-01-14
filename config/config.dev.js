97576b5d3c8c3ca2a64d026385e03e8089209e4a
龙 <oncwnuGyDkTgGakW2DB_mEUog6-A@git.weixin.qq.com>
2020-01-14 17:33:18 +0800

    xxx

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 const LOCAL_HOST = '127.0.0.1';
 const LOCAL_PORT = process.env.port || 3000;
 const SERV_HOST = '127.0.0.1';
@@ -9,20 +8,20 @@ export default {
   Local: {
     host: LOCAL_HOST,
     port: LOCAL_PORT,
-    domain: 'http://' + LOCAL_HOST + ':' + LOCAL_PORT
+    domain: `http://${LOCAL_HOST}:${LOCAL_PORT}`
   },
 
   // service env
   Service: {
     host: SERV_HOST,
     port: SERV_PORT,
-    domain: 'http://' + SERV_HOST + ':' + SERV_PORT
+    domain: `http://${SERV_HOST}:${SERV_PORT}`
   },
 
   Mysql: {
     host: 'localhost',
-    user: 'root',
+    user: 'long',
     password: '12345678',
     database: 'omygad'
   }
-}
+};
